dac5a163113278af460334c7508560139ed1d5b8
Garvin Hicking <hicking@faktor-e.de>
2025-07-18 13:24:40 +0200

    [TASK] T3v13 minimum example

--- Classes/Domain/Model/Singlefile.php
+++ Classes/Domain/Model/Singlefile.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace Garvinhicking\ExtbaseUpload\Domain\Model;
 
+use TYPO3\CMS\Extbase\Annotation\FileUpload;
 use TYPO3\CMS\Extbase\Domain\Model\FileReference;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 
 class Singlefile extends AbstractEntity
 {
+    // This below is the only code needed:
+    #[FileUpload([
+        'validation' => [
+            'maxFiles' => 1,
+        ],
+        'uploadFolder' => '1:/user_upload/',
+    ])]
     protected ?FileReference $file = null;
 
     public function getFile(): ?FileReference
